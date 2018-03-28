d3c299e5195648abb774b091e4c2b2850c5663bd
Iskander <iau.kgz@gmail.com>
2018-03-28 05:59:35 -0500

    1st commit

---
+++ lessson 1/script.js
@@ -0,0 +1,22 @@
+var budget = prompt ("Ваш бюджет?");
+var shopName = prompt ("Название вашего магазина?");
+var shopGoods = [prompt ("Какой тип товаров будем продавать?"), prompt ("Какой тип товаров будем продавать?"),
+prompt ("Какой тип товаров будем продавать?")];
+
+open = true;
+
+employers = {
+	manager: "John",
+	clerk: "Hannah",
+	secrurity: "Lev"
+}
+
+mainList = {
+	budget,
+	shopName,
+	shopGoods,
+	employers,
+	open
+}
+
+alert(budget/30);
